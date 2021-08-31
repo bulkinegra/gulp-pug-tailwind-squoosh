d7ae89b719f19f99dc462b58db2f7416b13158f4
bulkinegra <v.valentinvolkovwr@gmail.com>
2021-08-31 10:23:00 +0300

    наделал страничек

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     content: [
       './dist/*.html',
       './dist/*.js'
-  ]
+    ],
+    options: {
+      safelist: [
+        /data-theme$/,
+      ]
+    },
   },
   darkMode: 'media', // or 'media' or 'class'
 
@@ -22,7 +27,7 @@ module.exports = {
 
     fontFamily: {
       
-     }
+    }
   },
 
   variants: {
@@ -35,8 +40,39 @@ module.exports = {
     }
   },
   plugins: [
+    require('daisyui'),
     require('@tailwindcss/forms')({
       strategy: 'class',
     })
   ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
+// 'mytheme': {
+//   'primary': '#457B9D',
+//   'primary-focus': '#31607d',
+//   'primary-content': '#ffffff',
+//   'secondary': '#fd9f68',
+//   'secondary-focus': '#d78c60',
+//   'secondary-content': '#ffffff',
+//   'accent': '#e63946',
+//   'accent-focus': '#b7343f',
+//   'accent-content': '#ffffff',
+//   'neutral': '#1D3557',
+//   'neutral-focus': '#13253f',
+//   'neutral-content': '#ffffff',
+//   'base-100': '#ffffff',
+//   'base-200': '#f9fafb',
+//   'base-300': '#d1d5e0',
+//   'base-content': '#1f2937',
+//   'info': '#2094f3',
+//   'success': '#89d294',
+//   'warning': '#ff9900',
+//   'error': '#ff5724',
+// },
